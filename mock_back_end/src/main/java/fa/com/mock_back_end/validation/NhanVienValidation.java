d9337ff357db141705a56e379caf7d1cb145a56e
Anh Quang <anhquang0812@gmail.com>
2023-10-11 07:03:31 +0700

    resolve conflict

--- mock_back_end/src/main/java/fa/com/mock_back_end/validation/NhanVienValidation.java
+++ mock_back_end/src/main/java/fa/com/mock_back_end/validation/NhanVienValidation.java
@@ -1,11 +1,7 @@
 package fa.com.mock_back_end.validation;
 
-import fa.com.mock_back_end.dto.KhachHangDTO;
 import fa.com.mock_back_end.dto.NhanVienDTO;
-import fa.com.mock_back_end.entity.KhachHang;
 import fa.com.mock_back_end.entity.NhanVien;
-import fa.com.mock_back_end.repository.KhachHangRepository;
-import fa.com.mock_back_end.repository.NhanVienRepository;
 import fa.com.mock_back_end.service.NhanVienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,16 +24,22 @@ public class NhanVienValidation {
 
     public Map<String, String> validate(NhanVienDTO nhanVienDTO) {
         Map<String, String> errors = new HashMap<>();
-        Optional<NhanVien> nhanVien = nhanVienService.findById(nhanVienDTO.getMaNhanVien());
+        // Trườngg hợp password nhận về !null -> cần update password
+        if (nhanVienDTO.getPassword() == null ) {
+            if (nhanVienDTO.getNgaySinh().isAfter(LocalDate.now())) {
+                errors.put("ngaySinh", "Ngày sinh phải trước ngày hiện tại");
+            }
+            // nếu password, repassword khác null thì --> cập nhật lại password
+            return errors;
+        }
+        // Trườngg hợp password nhận về khác null -> cần update password
+            Optional<NhanVien> nhanVien = nhanVienService.findById(nhanVienDTO.getMaNhanVien());
         if (!Objects.equals(nhanVienDTO.getPassword(), nhanVienDTO.getRePassword())) {
             errors.put("rePassword", "Password không trùng nhau!");
         }
         if (nhanVien.isPresent()
                 && passwordEncoder.matches(nhanVienDTO.getPassword(), nhanVien.get().getMatKhau())) {
             errors.put("password", "Bạn hay thay đổi password!");
-        }
-        if (nhanVienDTO.getNgaySinh().isAfter(LocalDate.now())){
-            errors.put("ngaySinh","Ngày sinh phải trước ngày hiện tại");
         }
         return errors;
     }
